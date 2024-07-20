f1fd7d5f55c1be1e856be958881fc6a08879e110
linux1299 <1299258300@qq.com>
2024-07-20 09:21:00 +0800

    add clint

--- hw/spinal/dandriscv/gencpu/GenSuperScalar.scala
+++ hw/spinal/dandriscv/gencpu/GenSuperScalar.scala
@@ -6,5 +6,16 @@ import spinal.core._
 
 
 case class SuperScalar() extends Component {
-
+  val icache_config = ICacheConfig(
+    cacheSize = 32*1024, // 32KB
+    bytePerLine = 64,
+    wayCount = 4,
+    addressWidth = 32,
+    cpuDataWidth = 32,
+    bankWidth = 32,
+    busDataWidth = 64,
+    directOutput = false,
+    noBurst=true
+  )
+  val fetch = new Fetch(0x30000000, icache_config)
 }
